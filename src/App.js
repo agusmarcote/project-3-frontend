0fcac50f55c291380f26aac85e4611e69adc05f2
Matias Molina Bishop <molibi01@gmail.com>
2022-11-24 15:38:53 +0100

    routesapp.js

--- src/App.js
+++ src/App.js
@@ -20,6 +20,23 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/private" element={<IsPrivate><PrivateView /></IsPrivate>} />
             <Route path="*" element={<ErrorPage />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/create-event" element={<CreateEvent />} />
+            <Route path="/events/:eventId" element={<SingleEvent />} />
+            <Route path="/events/edit/:eventId" element={<EditEvent />} />
+            <Route path="/sales" element={<Sales />} />
+            <Route path="/create-sale" element={<CreateSale />} />
+            <Route path="/sales/:saleId" element={<SingleSale />} />
+            <Route path="/sales/edit/:saleId" element={<EditSale />} />
+            <Route path="/classes" element={<Classes />} />
+            <Route path="/create-class" element={<CreateClass />} />
+            <Route path="/classes/:classId" element={<SingleClass />} />
+            <Route path="/classes/edit/:classId" element={<EditClass />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/edit/:userId" element={<EditProfile />} />
+            <Route path="/profile/links" element={<ProfileLinks />} />
+            <Route path="/profile/links/edit" element={<ProfileLinksEdit/>} />
+
          </Routes>
       </div>
    );
