3edf043424fb6e1fa11d3888ee0f8f2c2d0bd069
Matias Molina Bishop <molibi01@gmail.com>
2022-11-30 13:34:08 +0100

    profile other accounts

---
+++ src/pages/profile/ProfileEventByID.js
@@ -0,0 +1,48 @@
+import "./ProfileLinks.css"
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
+
+const apiURL = "http://localhost:8000/api/v1/users/profile/"
+
+function ProfileEventByID() {
+
+    const { userId } = useParams()
+    const [user, setUser] = useState([])
+
+    useEffect(() => {
+        const apiCall = async () => {
+            const res = await axios.get(apiURL + userId)
+            console.log(res.data)
+            setUser(res.data)
+        }
+        apiCall()
+    }, [userId])
+
+    return (
+        <div className="profileSales profileSalesHeight">
+                {user.event && user.event.map((pro) => {
+                    console.log(pro)
+                    return (
+
+                        <div key={pro._id}>
+                            <Link className="cardLink flex" to={`/events/${pro._id}`}>
+                                <div className="CardStyle">
+                                    <img className="photoCard" src={pro.picture} alt="instrument" />
+                                    <h3 className="textStyle">{pro.title}</h3>
+                                    <p className="textStyle">{pro.instruments}</p>
+                                    <p className="textStyle"><i>{pro.description}</i></p>
+                                    <h4 className="priceStyleLits textStyle">price: <span className="spanPrice">${pro.price}</span></h4>
+                                    <img className="smallLogo" src="https:.tmimgcdn.com/scr/800x500/271800/equalizer-music-sound-logo-symbol-vector-v26_271868-original.jpg" alt="logo" />
+                                </div>
+                            </Link>
+                        </div>
+                    )   
+                })}
+        </div>
+    )
+}
+
+
+export default ProfileEventByID
