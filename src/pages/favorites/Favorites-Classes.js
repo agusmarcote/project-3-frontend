40efb9a9edd6844bacfff0c8dfc5c29f3b1f93a0
proyectomarco <marco.bravo0022@gmail.com>
2022-12-01 11:48:41 +0100

    favorites finally done

--- src/pages/favorites/Favorites-Classes.js
+++ src/pages/favorites/Favorites-Classes.js
@@ -1,19 +1,23 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
+import '../favorites/Favorites-X.css'
+import { Link } from "react-router-dom"
 
 const apiURL = 'http://localhost:8000/api/v1/favorites/favorites'
 
+
 export default function Favorites() {
     const [user, setUser] = useState(null)
-    
-    
+    const [classes, setClasses] = useState([])
+
+
     useEffect(() => {
         const apiCall = async () => {
             const storedToken = localStorage.getItem("authToken");
             try {
-                const res = await axios.get(apiURL, { headers: { Authorization: `Bearer ${storedToken}` }})
+                const res = await axios.get(apiURL, { headers: { Authorization: `Bearer ${storedToken}` } })
                 console.log(res.data)
-                setUser(res.data) 
+                setUser(res.data)
             } catch (error) {
                 console.log(error)
             }
@@ -23,16 +27,22 @@ export default function Favorites() {
 
     return (
         <div>
-            {user && user.favoriteClass.map((el)=> {
+            {user && user.favoriteClass.length == 0 && <h1 className="noFav">There are no favorites saved yet</h1>}
+            {user && user.favoriteClass.map((el) => {
                 return (
-                    <div>
-                        <img src={el.picture} />
-                        <h1>{el.title}</h1>
-                        <p>{el.description}</p>
+                    <div className="favoriteX">
+                    <div className="favoriteClass">
+                        <Link to={`/classes/${el._id}`}>
+                            <img className="photoCard" src={el.picture} alt="Favorites Classes" />
+                            <br />
+                            <h1 className="textStyle">{el.title}</h1>
+                            <p className="textStyleHome">{el.description}</p>
+                            <p className="spanPrice">{el.price}€</p>
+                        </Link>
+                    </div>
                     </div>
                 )
             })}
-
         </div>
     )
 }
